b75f04df869311cca64bf6d6a9945085c7e89a78
jonathan <jonathanariel94@gmail.com>
2016-05-14 21:14:59 -0300

    Jonathan-headers

---
+++ Piso.h
@@ -0,0 +1,11 @@
+#ifndef PISO_H_INCLUDED
+#define PISO_H_INCLUDED
+
+typedef struct{
+   list<Ubicacion> ubicaciones;
+} Piso;
+
+void addEstante(Articulo &articulo, Articulo &articulo);
+
+
+#endif // PISO_H_INCLUDED
--- articulo.h
+++ articulo.h
@@ -1,6 +1,22 @@
 #ifndef ARTICULO_H_INCLUDED
 #define ARTICULO_H_INCLUDED
 
-typedef struct Artiulo;
+typedef struct{
+    int codArticulo;
+    float precio;
+    std::string descripcion;
+} Articulo;
+
+void setCodArticulo(Articulo &articulo, int codArticulo);
+
+void setPrecio(Articulo &articulo, float precio);
+
+void setDescripcion(Articulo &articulo, std::string descripcion);
+
+int getCodArticulo(Articulo &articulo);
+
+float getPrecio(Articulo &articulo);
+
+std::string getDescripcion(Articulo &articulo);
 
 #endif // ARTICULO_H_INCLUDED
